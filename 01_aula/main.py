7386db0322fac5a136429ba62501bb5bb2f1050d
WesRoch <wesleyroodrigo@gmail.com>
2023-05-04 18:14:22 -0300

    reordering venv

--- 01_aula/main.py
+++ 01_aula/main.py
@@ -1,2 +1,18 @@
 import requests
 import pandas as pd
+import sys
+import collections
+
+url = 'https://servicebus2.caixa.gov.br/portaldeloterias/api/resultados?modalidade=Lotofácil'
+
+r = requests.get(url)
+
+r.text
+r_text = r.text
+
+df = pd.read_html(r_text)
+
+type(df)
+type(df[0])
+
+df=df[0].copy()
