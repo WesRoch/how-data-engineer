ef255aa297d879c3f09efc52d4360ded26406537
Wesley Rocha <wesleyroodrigo@gmail.com>
2023-06-02 18:56:07 -0300

    using log, how to return log info and error message

--- 03_aula/main.py
+++ 03_aula/main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import backoff
 import random
-
+import logging
 #%%
 url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
 
@@ -111,12 +111,12 @@ multi_moeda(30, 'EUR-BRL')
 # usando a library backoff
 
 @backoff.on_exception(backoff.expo,(ConnectionAbortedError, ConnectionRefusedError, TimeoutError))
-def test_func(*args, **kargs):
+def test_func(*args, **kwargs):
     rnd = random.random()
     print(f"""
           RND: {rnd}
           args: {args if args else 'sem args'}
-          kargs: {kargs if kargs else 'sem kargs'} 
+          kwargs: {kwargs if kwargs else 'sem kwargs'} 
           """)
     if rnd < .2: # refazendo as tentativas e retornando os erros
         raise ConnectionAbortedError("Conexão foi finalizada")
@@ -133,4 +133,42 @@ test_func()
 test_func(30)
 # %%
 test_func(30, 24, nome='wesley')
+# %%
+# Logs
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+# que informações vamos apresentar
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s' # time, name = usuario do terminal
+    )
+ch = logging.StreamHandler() # canal 
+ch.setFormatter(formatter) # carregando o formato no canal
+log.addHandler(ch) # adicionando o canal de saída a log
+# %%
+
+@backoff.on_exception(backoff.expo,(ConnectionAbortedError, ConnectionRefusedError, TimeoutError))
+def test_func_error(*args, **kwargs):
+    rnd = random.random()
+    log.debug(f" RND: {rnd} ")
+    log.info(f" {args if args else 'sem args'}")
+    log.info(f"{kwargs if kwargs else 'sem kwargs'}")
+    if rnd < .2: # refazendo as tentativas e retornando os erros
+        log.error("Conexão foi finalizada.")
+        raise ConnectionAbortedError("Conexão foi finalizada")
+    elif rnd < .4:
+        log.error("Conexão foi recusada.")
+        raise ConnectionRefusedError("Conexao foi recusada.")
+    elif rnd < .6:
+        log.error("Tempo de espera excedido.")
+        raise TimeoutError("Tempo de espera excedido.")
+    else:
+        return "OK!"
+# %%
+
+# util para rastrear os processos, ver tudo o que foi executado, tipo de erro etc
+# capturando uma carga -> cai no servidor -> importa -> imprime qtd de registro, linhas, estatisticas
+# depois sobe pro banco, tendo um track de tudo o que aconteceu, sendo um arquivo gerado ou mostrando
+# pro usuario
+test_func_error()
 # %%
