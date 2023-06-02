e3af515d43e4463c8feff6b0d498310789746343
Wesley Rocha <wesroch@wesleyPC>
2023-06-02 15:53:10 -0300

    api requests get 200

--- 03_aula/main.py
+++ 03_aula/main.py
@@ -1,14 +1,35 @@
 #%%
 # import requests
 import requests
+import json
 
 #%%
-url = 'http://www.google.com'
+url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
 
 ret = requests.get(url)
 
 #%%
 
-print(ret)
+print(ret) # response 200 - success
 
+# %%
+
+print(ret.text)
+# %%
+
+url_erro = 'https://economia.awesomeapi.com.br/json/wrr/USD-BRL'
+
+ret_erro = requests.get(url_erro)
+# %%
+
+if ret_erro:
+    print(ret_erro) ## caso a request fosse certa, retornaria a variavel com a response 200
+else:
+    print('Falhou,', ret_erro) ## irá retornar 404, requisição aonde n existe
+# %%
+
+dolar = json.loads(ret.text)['USDBRL']
+# %%
+
+print( f"20 Dólares hoje custam {float(dolar['bid']) * 20} reais")
 # %%
